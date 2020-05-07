6d7f7a9d54a2448f896d233798033416d4030fa3
Potapov Evgeny <foxspb2012@gmail.com>
2020-05-07 18:57:57 +0300

    Выполнил первое задание

---
+++ source/js/index.js
@@ -0,0 +1,26 @@
+  var mainNav = '.main-nav';
+  var mainMenu = '.main-menu';
+  var noJS = 'main-nav--no-js';
+  var menuClosed = 'main-menu--menu-closed';
+  var btnToggle = 'main-nav__toggle';
+  var btnToggleOff = 'main-nav__toggle--off';
+
+  var mainNav = document.querySelector(mainNav);
+  var mainMenu = document.querySelector(mainMenu);
+
+  if (mainNav && mainNav.classList.contains(noJS)) {
+    mainNav.classList.remove(noJS);
+
+    if (mainMenu) {
+      mainMenu.classList.add(menuClosed);
+    }
+  }
+
+  mainNav.addEventListener('click', function (event) {
+    if (event.target.classList.contains(btnToggle)) {
+      event.preventDefault();
+
+      mainMenu.classList.toggle(menuClosed);
+      event.target.classList.toggle(btnToggleOff);
+    }
+  });
